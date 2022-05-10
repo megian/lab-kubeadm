e6b3a434ff6714c6815edddd57105b76ae8f1c98
Megian <gabisoft@freesurf.ch>
2022-05-10 19:31:15 +0200

    Disable meanwhile cilium native routing

--- role/cilium.sh
+++ role/cilium.sh
@@ -10,12 +10,16 @@ helm upgrade --install cilium cilium/cilium --version 1.11.1 \
     --namespace kube-system \
     --set kubeProxyReplacement=strict \
     --set k8sServiceHost=vip.$(hostname -d) \
-    --set k8sServicePort=6443 \
-    --set ipam.mode=kubernetes \
-    --set tunnel=disabled \
-    --set enable-endpoint-routes=true \
-    --set enable-local-node-route=false
+    --set k8sServicePort=6443
 
+# Enable native routing
+#
+#    --set ipam.mode=kubernetes \
+#    --set tunnel=disabled \
+#    --set enable-endpoint-routes=true \
+#    --set enable-local-node-route=false
 #    --set native-routing-cidr=10.0.0.0/16 \
+#
+# Requires --allocate-node-cidrs on the kubernetes controller
 
 # cilium status --wait
