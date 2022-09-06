0847d7a832cb669a3cfea3d74c0a2c6634bdcfae
Gabriel Mainberger <gabisoft@freesurf.ch>
2022-09-06 15:14:03 +0200

    Add postconfig control plane taint deletion

--- role/postconfig.sh
+++ role/postconfig.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
+
+# Remove control-plane taints
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl taint nodes --all node-role.kubernetes.io/master-
