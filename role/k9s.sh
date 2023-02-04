7e0a682df9f1fb72cd882ab325a3e5d14eebb309
Gabriel Mainberger <gabisoft@freesurf.ch>
2023-02-04 17:20:22 +0100

    Update k9s filename

--- role/k9s.sh
+++ role/k9s.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 k9s_version="${1:-v0.26.3}"; shift || true
 
 # download and install.
-wget -qO- "https://github.com/derailed/k9s/releases/download/$k9s_version/k9s_Linux_x86_64.tar.gz" \
+wget -qO- "https://github.com/derailed/k9s/releases/download/$k9s_version/k9s_Linux_amd64.tar.gz" \
   | tar xzf - k9s
 install -m 755 k9s /usr/local/bin/
 rm k9s
