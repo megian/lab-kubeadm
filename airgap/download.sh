585ca9eaa1aa6ba0ebdef8cc4193638ac30eadd6
Gabriel Mainberger <gabisoft@freesurf.ch>
2023-05-10 13:25:51 +0200

    Update airgap to Kubernetes 1.27

--- airgap/download.sh
+++ airgap/download.sh
@@ -4,14 +4,14 @@
 # root@n1:~# kubeadm config images pull
 # root@n1:~# crictl images
 # IMAGE                                TAG                 IMAGE ID            SIZE
-# ghcr.io/kube-vip/kube-vip                 v0.5.0              09067696476ff       37.9MB
-# quay.io/cilium/cilium                     <none>              526bd4754c9cd       460MB
-# quay.io/cilium/operator-generic           <none>              b7eda471b44d1       63MB
-# registry.k8s.io/coredns/coredns           v1.9.3              5185b96f0becf       48.9MB
-# registry.k8s.io/etcd                      3.5.4-0             a8a176a5d5d69       301MB
-# registry.k8s.io/kube-apiserver            v1.25.0             4d2edfd10d3e3       129MB
-# registry.k8s.io/kube-controller-manager   v1.25.0             1a54c86c03a67       118MB
-# registry.k8s.io/kube-proxy                v1.25.0             58a9a0c6d96f2       63.3MB
-# registry.k8s.io/kube-scheduler            v1.25.0             bef2cf3115095       51.9MB
+# ghcr.io/kube-vip/kube-vip                 v0.5.12             1227540e2a615       41.7MB
+# quay.io/cilium/cilium                     <none>              2480b5e5b4799       486MB
+# quay.io/cilium/operator-generic           <none>              5e08056ff5956       74.4MB
+# registry.k8s.io/coredns/coredns           v1.10.1             ead0a4a53df89       53.6MB
+# registry.k8s.io/etcd                      3.5.7-0             86b6af7dd652c       297MB
+# registry.k8s.io/kube-apiserver            v1.27.1             6f6e73fa8162b       122MB
+# registry.k8s.io/kube-controller-manager   v1.27.1             c6b5118178229       114MB
+# registry.k8s.io/kube-proxy                v1.27.1             fbe39e5d66b6a       72.7MB
+# registry.k8s.io/kube-scheduler            v1.27.1             6468fa8f98696       59.8MB
 # registry.k8s.io/pause                     3.6                 6270bb605e12e       690kB
-# registry.k8s.io/pause                     3.8                 4873874c08efc       718kB
+# registry.k8s.io/pause                     3.9                 e6f1816883972       750kB
